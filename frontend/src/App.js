8f59ce10b104ed8bd9df98fde073021684e178ac
Valeria Muhembele <61202849+anamivale@users.noreply.github.com>
2022-08-18 20:58:09 +0300

    map trial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,38 @@
-import React from "react"
-// import Home from "./components/home"
-// import { BrowserRouter, Routes, Route } from "react-router-dom"
-// import About from "./components/About"
-// import Login from "./components/Login"
-// import Contact from "./components/Contact"
-import Map from "./components/map"
+import React, { useState } from "react"
+import Home from "./components/home"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import About from "./components/About"
+import Login from "./components/Login"
+import Contact from "./components/Contact"
+import jwtDecode from "jwt-decode"
+// import SimpleMap from "./components/map"
+let logUser
+if (localStorage.token) {
+  const jwt = localStorage.getItem("token")
+  logUser = jwtDecode(jwt)
+}
 
 function App() {
+  const [user, setUser] = useState(logUser)
+  if (!user) {
+    return (
+      <div>
+        <Login />
+      </div>
+    )
+  }
+
   return (
     <div className="App">
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/login" element={<Login />} />
+
           <Route exact path="/About" element={<About />} />
           <Route exact path="/Contact" element={<Contact />} />
         </Routes>
-      </BrowserRouter> */}
-      <Map />
+      </BrowserRouter>
     </div>
   )
 }
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,14 +1,25 @@
 import React from "react"
+import "../css/login.css"
 
 function Login() {
   return (
-    <div>
-      <div>
-        <select name="" id="">
-          <op>login as user</op>
-          <op>login as Admin</op>
-        </select>
-      </div>
+    <div className="login">
+      <form className="form">
+        <label htmlFor="email">Email</label>
+        <br />
+        <input type="email" id="" />
+        <br />
+        <label htmlFor="password">Password</label>
+        <br />
+        <input type="password" id="" />
+        <br />
+        <div className="btn-login">
+          <button>Login</button>
+        </div>
+        <p>
+          Don't have an account yet? <a href="#">register here</a>
+        </p>
+      </form>
     </div>
   )
 }
