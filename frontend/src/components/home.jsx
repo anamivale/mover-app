ef3060da963cad5eee2dfc8f34f84e16aadcf747
Valeria Muhembele <61202849+anamivale@users.noreply.github.com>
2022-08-31 15:07:47 +0300

    order page

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -13,20 +13,44 @@ function Home() {
           <NavLink to="/login" className="link">
             Login
           </NavLink>
-          <NavLink to="/About" className="link">
+          <a href="#about" className="link">
             About
-          </NavLink>
-          <NavLink to="/Contact" className="link">
+          </a>
+          <a href="#contact" className="link">
             Contact
-          </NavLink>
+          </a>
         </div>
       </nav>
       <div>
         <h1>
           OUR MISSION: PROVIDE MOVING TIPS THAT SAVE YOU TIME AND MONEY DURING
-          THE JOURNEY INTO YOU ARE NEW HOME OR OFFICE!
+          THE JOURNEY INTO YOUr NEW HOME OR OFFICE!
         </h1>
-        <img src="../images/truck1.png" alt="truck" />
+        <div className="img"></div>
+      </div>
+      <div id="about">
+        <h1 className="about">About us</h1>
+        <p>
+          We are to provide services for the residence of kisumu town shifting
+          from one place to anoter but within kisumu toen. in future we can
+          extend to other town.
+        </p>
+        <p>
+          Our Vision and Core Values Cube Movers will lead in defining
+          excellence in relocation by ensuring quality service delivery and
+          delight. Honesty Courtesy Creativity Teamwork Enthusiasm Care
+        </p>
+      </div>
+      <div id="contact">
+        <h1 className="about">Contact Us</h1>
+        <div className="contact">
+          <p>
+            Email: muhembelev@gmail.com <br /> contact:+254797936562
+          </p>
+          <p>
+            Email: muhembelev@gmail.com <br /> contact:+254797936562
+          </p>
+        </div>
       </div>
     </div>
   )
