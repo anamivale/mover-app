31c84363e04277151b9b60c1ede3d777b0853053
Valeria Muhembele <61202849+anamivale@users.noreply.github.com>
2022-09-18 00:13:45 +0300

    user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,20 +1,29 @@
 const router = require("express").Router()
 const User = require("../models/user")
 const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
 
 router.get("/", (req, res) => {
   res.send("first route")
 })
 
 router.post("/user", async (req, res) => {
-  const { name, password, email, contact } = req.body
+  const { name, email, contact } = req.body
   try {
-    const user = new User(req.body)
+    const bpass = (await bcrypt.hash(req.body.password, 10)).toString()
+    const user = new User({
+      name: req.body.name,
+      password: bpass,
+      email: req.body.email,
+      contact: req.body.contact,
+    })
     await user.save()
-    const token = jwt.sign(process.env.sec, { password, email })
-    res.send("added")
+    const token = jwt.sign({ email }, process.env.sec, {
+      expiresIn: "1h",
+    })
+    res.status(200).json({ name, contact, token })
   } catch (error) {
-    console.log(error[0])
+    res.status(500).json("error")
   }
 })
 
