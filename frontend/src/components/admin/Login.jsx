62cc8e0a7ef0cac6c43f08ad38259b64831bec16
Valeria Muhembele <61202849+anamivale@users.noreply.github.com>
2022-10-18 11:08:34 +0300

    login page

--- frontend/src/components/admin/Login.jsx
+++ frontend/src/components/admin/Login.jsx
@@ -1,27 +1,50 @@
 import React from "react"
+import { useState } from "react"
 import { NavLink } from "react-router-dom"
+import axios from ('axios')
 import "./css//login.css"
 
 function Login() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+  }
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const user = { email, password }
+
+  try {
+    axios.post("http://localhost:4002/")
+  } catch (error) {
+    
+  }
+  }
+
   return (
     <div className="login">
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <label htmlFor="email">Email</label>
         <br />
-        <input type="email" id="email" />
+        <input type="email" id="email" value={email} onChange={handleEmail} />
         <br />
         <label htmlFor="password">Password</label>
         <br />
-        <input type="password" id="password" />
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePassword}
+        />
         <br />
         <div className="btn-login">
-          <NavLink to="/" className="link">
-            <button>Login</button>
-          </NavLink>
+          {/* <NavLink to="/admin" className="link"> */}
+          <button>Login</button>
+          {/* </NavLink> */}
         </div>
-        <p>
-          Don't have an account yet? <NavLink to="/login"> Signup</NavLink>
-        </p>
       </form>
     </div>
   )
