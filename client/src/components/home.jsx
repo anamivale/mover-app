5de947802e7e3da672e7c52b0c7287234e2beebb
Valeria Muhembele <61202849+anamivale@users.noreply.github.com>
2022-11-03 08:46:16 +0300

    services

--- client/src/components/home.jsx
+++ client/src/components/home.jsx
@@ -1,5 +1,13 @@
 import React from "react"
 import Track1 from "../images/truck1.png"
+import HOmeTruck from "../images/sizes.jpg"
+import Track2 from "../images/1000-100.jpg"
+import Track3 from "../images/1300-100.jpg"
+import Track4 from "../images/2000-100.jpg"
+import Track5 from "../images/1700.jpg"
+import Track6 from "../images/3500.jpg"
+import logo from "../images/logo.jpg"
+
 import { NavLink } from "react-router-dom"
 import "../css/home.css"
 import { useState } from "react"
@@ -30,7 +38,9 @@ function Home() {
     <div className="home">
       <nav>
         <div className="ggg">
-          <h1 className="logo">Easy Moving</h1>
+          <div className="logo">
+            <img src={logo} alt="" className="logo" />
+          </div>
         </div>
         <div>
           {/* <NavLink to="/login" className="link">
@@ -49,37 +59,78 @@ function Home() {
       </nav>
       <div>
         <h1 className="slogun">
-          To save time and get moving tips, always come to easy moving. Easy
-          moving <br /> makes your moving faster and enjoyable.
+          Enjoy reliable moving with Easy Movers. <br /> Save time, move easy
+          and fast!!!
         </h1>
         <div className="img"></div>
       </div>
       <div id="about">
         <h1 className="about">About us</h1>
         <p>
-          We are to provide services for the residence of kisumu town shifting
-          from one place to anoter but within kisumu town. In future we can
-          extend to other towns.
+          Easy Movers provide relocation services within kisumu town. We are
+          working on extending our services to other locations outside kisumu
+          town.
         </p>
         <p>
-          Our Vision and Core Values Easy Movers will lead in defining
-          excellence in relocation by ensuring quality service delivery and
-          delight. Honesty Courtesy Creativity Teamwork Enthusiasm Care
+          Our vision as Easy movers is to lead, redefine and making relocation
+          services undemanding and being the most efficient and reliable
+          conveyers. We have a dependable team which is happy and ready to serve
+          you. <br />
+          Our core values are honesty, reliability , teamwork and enthusiasm.
         </p>
       </div>
       <div id="services">
         <h1 className="service-title">Services Offered</h1>
+        <div className="size">
+          <figure>
+            <img src={HOmeTruck} alt="truck" />
+          </figure>
+        </div>
         <div className="service">
           <div className="pkg1">
             <figure>
-              <img src={Track1} alt="" />
-              <figcaption> price: 3000/trip </figcaption>
+              <img src={Track2} alt="" />
+              <figcaption>
+                {" "}
+                14 by 22 m3 truck @ kshs:1000 for every 100m
+              </figcaption>
             </figure>
           </div>
           <div className="pkg2">
             <figure>
-              <img src={Track1} alt="" />
-              <figcaption> price: 4500/trip </figcaption>
+              <img src={Track3} alt="" />
+              <figcaption>
+                {" "}
+                22 by 32 m3 truck @ kshs:1300 for every 100m{" "}
+              </figcaption>
+            </figure>
+          </div>
+          <div className="pkg2">
+            <figure>
+              <img src={Track4} alt="" />
+              <figcaption>
+                {" "}
+                40 by 59 m3 truck @ kshs:2000 for every 100m{" "}
+              </figcaption>
+            </figure>
+          </div>
+          <div className="pkg2">
+            <figure>
+              <img src={Track5} alt="" />
+              <figcaption>
+                {" "}
+                32 by 40 m3 truck @ kshs:1700 for every 100m{" "}
+              </figcaption>
+            </figure>
+          </div>
+          <div className="pkg2">
+            <figure>
+              <img src={Track6} alt="" />
+              <figcaption>
+                {" "}
+                Tripper truck @ kshs 3500 for one trip + kshs 2000 for any extra
+                trip
+              </figcaption>
             </figure>
           </div>
         </div>
